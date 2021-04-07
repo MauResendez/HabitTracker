e307499d78262a7a1cfee1e2df060078ca8ec1f4
Ab3pAydAy <salinas772089@yahoo.com>
2021-04-07 16:49:21 -0500

    add app test

--- test_driver/app.dart
+++ test_driver/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_driver/driver_extension.dart';
-import 'package:homebrew_sum2/main.dart' as app;
+import 'package:habittracker/main.dart' as app;
 
 void main() {
   // This line enables the extension.
